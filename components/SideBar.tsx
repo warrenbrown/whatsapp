8d02e6a492ffc56b43e2cef20759c501b4d4f0f7
Warren Brown <warren@katapultmedia.com>
2025-04-02 11:23:22 -0500

    feat: add new pages and components

---
+++ components/SideBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SideBar = () => {
+    return (
+        <div>
+            {/* Header */}
+            {/* Search Bar */}
+            {/* List of Chat elements */}
+
+
+
+            
+        </div>
+    )
+}
+
+export default SideBar
